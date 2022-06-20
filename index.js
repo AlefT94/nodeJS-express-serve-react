58e3ae81a5e29b409975823ad78eb6d4e2b10926
Alef Teodoro <alef_teodoro1@hotmail.com>
2022-06-20 15:43:13 -0300

    ajustado o arquivo de build, inserindo um exemplo com routes

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 
 
-app.get('/', (req,res)=>{
+app.use('/',express.static(path.join(__dirname,'build')));
+
+app.get('/*', (req,res)=>{
   //aqui é realizado o envio do index. caso o mesmo não seja carregado, a página não é crida e nenhuma das rotas poderá ser acessada
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-//para que o códgo do get '/' seja executado, é necessário que essa configuração fique abaixo.
-app.use('/',express.static(path.join(__dirname,'build')));
 
 app.listen(3001,()=>{
   console.log(`Example app listening on port 3001`)
